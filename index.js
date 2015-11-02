bc42c0223da2b2415523d728750f1563388c4d61
Xiphe <github@xiphe.net>
2015-11-03 00:35:27 +0100

    refactor(forEachToolbox): use package name as task name
    in order to keep thinks as dry as possible and enforce clear naming
    
    tasks are now registered in this project, toolboxes only provie
    task factories

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var path = require('path');
-var _ = require('lodash');
 var di = require('di');
 var validateConfig = require('./lib/validateConfig');
 var setDefaults = require('./lib/setDefaults');
 var groupie = require('./lib/groupie');
+var forEachToolbox = require('./lib/forEachToolbox');
 
 function getInjector(config) {
   return new di.Injector([{
@@ -13,29 +12,17 @@ function getInjector(config) {
   }]);
 }
 
-function getToolboxes(config) {
-  var toolboxes = [];
-
-  if (config.pkg.devDependencies) {
-    _.forEach(config.pkg.devDependencies, (version, devDependency) => {
-      if (devDependency.startsWith('gulp-toolbox-')) {
-        toolboxes.push(require(path.join(
-          config.basePath,
-          'node_modules',
-          devDependency
-        )));
-      }
-    });
-  }
-
-  return toolboxes;
-}
-
 module.exports = function(config) {
   validateConfig(config);
   setDefaults(config);
   var injector = getInjector(config);
 
-  getToolboxes(config).forEach(toolbox => injector.invoke(toolbox));
+  forEachToolbox(config, (toolboxName, taskFactory) => {
+    config.gulp.task(
+      toolboxName,
+      injector.invoke(taskFactory)
+    );
+  });
+
   groupie.run(config);
 };
