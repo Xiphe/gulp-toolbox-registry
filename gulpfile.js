fbe9939f04203b9b72f168eea70f03dc364ba6a5
Xiphe <github@xiphe.net>
2016-02-22 18:26:22 +0100

    feat(PluginDecoratePipes): add .getPipe method to helper
    providing pipes, previously registered as config.pipes

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@
 var gulp = require('gulp');
 var GulpToolboxRegistry = require('./index');
 var testNodeJasmine = require('gulp-toolbox-test-node-jasmine');
-// var optionalCoverageIstanbul = require(
-//   'gulp-toolbox-optional-coverage-istanbul'
-// );
+var pipeCoverageIstanbul = require('gulp-toolbox-pipe-coverage-istanbul');
 
 gulp.registry(new GulpToolboxRegistry({
   toolboxes: [
     testNodeJasmine
-    // optionalCoverageIstanbul
+  ],
+  pipes: [
+    pipeCoverageIstanbul
   ],
   config: {
     files: {
